c070a501c734f474bbc5f1d743cbb3ce6a0561d5
fushuli <fushuli@hujiang.com>
2018-11-26 00:01:42 +0800

    base functionalty

--- src/App.js
+++ src/App.js
@@ -3,26 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: 0
+    }
+  }
+
+  add = () => {
+    this.setState({
+      count: this.state.count + 1 
+    })
+  }
+
   render() {
+    const { count } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div>
+        I'm a React Application
+        <hr />
+        <button onClick={this.add}>click me to add</button>  
+        <hr />
+        <Box count={count} />
+      </div>)
   }
 }
+
+const Box = props => <div>The current number is:{props.count}</div>
 
 export default App;
