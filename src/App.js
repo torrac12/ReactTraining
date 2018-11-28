fa4bcdc968143e5b4ad2ef0cdfaf5bbe8c89777a
fushuli <fushuli@hujiang.com>
2018-11-28 23:49:10 +0800

    1-use ref to handle dom node api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import demoAudio from './demo.mp3'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      width: 0
+    }
+  }
+
+  componentDidMount() { // think about it why should do in this lifeCycle
+    this.setState({
+      width: this.node.clientWidth
+    })
+
+    this.audio.play()
+
+  }
+
+  getNode = (node) => {
+    this.node = node;
+  }
+
+  getAudio = audioNode => {
+    this.audio = audioNode
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="App" ref={this.getNode} >
+        <header className="App-header" >
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,6 +44,8 @@ class App extends Component {
           >
             Learn React
           </a>
+          <h3>img’s width is {this.state.width}</h3>
+          <audio src={demoAudio} controls  ref={this.getAudio}/> 
         </header>
       </div>
     );
