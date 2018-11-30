4749ce7ed2663119a091a5e7e0512ff17ca0cc97
Julia Fu <julia.fu@backstopdevelopement.com>
2018-11-30 14:04:49 +0800

    1-propTypes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,10 +20,40 @@ class App extends Component {
           >
             Learn React
           </a>
+          <PropTypesChecking optionalArray='sss' optionalNode={() => (<div>2</div>)} />
         </header>
       </div>
     );
   }
 }
+
+const PropTypesChecking = props => <div>PropTypesChecking</div>
+
+PropTypesChecking.propTypes = {
+  // You can declare that a prop is a specific JS type. By default, these
+  // are all optional.
+  optionalArray: PropTypes.array,
+  optionalBool: PropTypes.bool,
+  optionalFunc: PropTypes.func,
+  optionalNumber: PropTypes.number,
+  optionalObject: PropTypes.object,
+  optionalString: PropTypes.string,
+  requiredString:  PropTypes.string.isRequired,
+
+  // Anything that can be rendered: numbers, strings, elements or an array
+  // (or fragment) containing these types.
+  optionalNode: PropTypes.node,
+
+  // A React element.
+  optionalElement: PropTypes.element,
+
+  // You can ensure that your prop is limited to specific values by treating
+  // it as an enum.
+  optionalEnum: PropTypes.oneOf(['News', 'Photos']),
+
+  // An array of a certain type
+  optionalArrayOf: PropTypes.arrayOf(PropTypes.number),
+
+};
 
 export default App;
